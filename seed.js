a30eaf1970058d0736931b5f1e578268743a2553
mbd-s <michaelbsilverberg@gmail.com>
2016-04-02 13:08:23 -0700

    updated seed.js, added a profile schema

--- seed.js
+++ seed.js
@@ -1,15 +1,22 @@
 // This file allows us to seed our application with data
 // simply run: `node seed.js` from the root of this project folder.
 
-// var db = require('./models');
+var db = require('./models');
 
-// var new_campsite = {description: "Sharp rocks. Middle of nowhere."}
+var new_profile = {
+  name: "Michael Silverberg",
+  github_link: "https://github.com/mbd-s",
+  github_profile_image: "https://avatars0.githubusercontent.com/u/13588399",
+  current_city: "Berlin",
+  pets: [
+    {name: "Moku", type: "cat", color: "black"},
+    {name: "Frances", type: "cat", color: "grey"}]
+};
 
-// db.Campsite.create(new_campsite, function(err, campsite){
-//   if (err){
-//     return console.log("Error:", err);
-//   }
-
-//   console.log("Created new campsite", campsite._id)
-//   process.exit(); // we're all done! Exit the program.
-// })
+db.Profile.create(new_profile, function(err, profile){
+  if (err){
+    return console.log("Error:", err);
+  }
+  console.log("Created new profile", profile._id);
+  process.exit();
+});
