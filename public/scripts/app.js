5b03191cfe51db7f5b230d8dbbc0302ae9ae1265
mbd-s <michaelbsilverberg@gmail.com>
2016-04-02 16:24:36 -0700

    profile endpoint working on local host

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -10,11 +10,27 @@ $.ajax({
 });
 
 function onSuccess(json){
-  $('#profile-display').text(json);
+  $('#profileDisplay').append('<div><ul><li>' + json[0].name + '</li><li>' +
+  '<a href="' + json[0].github_link + '">Github</a></li></ul></div>');
 }
 
 function onError(e) {
   console.log('uh oh');
-  $('#profile-display').text('Failed to load profile.');
+  $('#profileDisplay').append('Failed to load profile.');
 }
 });
+
+// var new_profile = {
+//   name: "Michael Silverberg",
+//   github_link: "https://github.com/mbd-s",
+//   github_profile_image: "https://avatars0.githubusercontent.com/u/13588399",
+//   current_city: "Berlin",
+//   pets: [
+//     {
+//       name: "Moku", type: "cat", color: "black"
+//     },
+//     {
+//       name: "Frances", type: "cat", color: "grey"
+//     }
+//   ]
+// };
